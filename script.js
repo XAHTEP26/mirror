325dc5d5991367a93d0a77d1122a1d007b2a608d
XAHTEP26 <89197353165@mail.ru>
2019-06-16 11:24:46 +0300

    Change domain. Remove scope. Disable audio.

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+navigator.mediaDevices.getUserMedia({ video: true, audio: false })
   .then(stream => document.querySelector('.video').srcObject = stream);
--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
 workbox.core.setCacheNameDetails({
-  prefix: 'kogri',
+  prefix: 'kogri-workertest',
   suffix: 'v1',
   precache: 'precache',
   runtime: 'runtime'
 });
 
 workbox.precaching.precacheAndRoute([
-  { url: '/workertest/index.html', revision: '4' },
-  { url: '/workertest/styles.css', revision: '4' },
-  { url: '/workertest/script.js', revision: '4' }
+  { url: '/workertest/index.html', revision: '5' },
+  { url: '/workertest/styles.css', revision: '5' },
+  { url: '/workertest/script.js', revision: '5' }
 ]);
